b66c2e7a25c4c86658586638c4d3beafcfe9a259
RafaelVargazz <vargas.rafael.art@gmail.com>
2026-02-10 20:15:04 -0300

    Update basic_math.dart

--- lib/basic_math.dart
+++ lib/basic_math.dart
@@ -1,15 +1,15 @@
 int somar(int num1, int num2) {
- 
+ return num1 + num2;
 }
 
 double subtrair(double num1, double num2) {
-  
+  return num1 - num2;
 }
 
 double multiplicar(double num1, double num2) {
-  
+  return num1 * num2;
 }
 
-String formatarParaMonetario(double valor) {
-  
+String? formatarParaMonetario(double valor) {
+ return "R\$${valor}";
 }
